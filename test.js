d269dd7982435cd096360e4bd550c284c969bfcf
untilArch <dpanco@saic.edu>
2016-06-12 11:51:21 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+var text = 'do,do,do,do,Cora,do,do,Cora'
+var myName = 'Cora'
+var hits = []
+
+for (var i = 0; i < text.length; i++) {
+  if (text[i] === 'C') {
+    for (var j = i; j < (i + myName.length); j++) {
+      hits.push(text[j])
+    }
+  }
+}
+if (hits.length === 0) {
+  console.log("Your name wasn't found!")
+} else {
+  console.log(hits)
+}
